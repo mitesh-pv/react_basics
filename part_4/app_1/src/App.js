003c5bfdd8d10e3433cc04577171f6beb083d4d4
mitesh_pv <mitesh_pv@mitesh-pvs-MacBook-Pro.local>
2019-06-22 22:29:15 +0530

    component nesting

--- part_4/app_1/src/App.js
+++ part_4/app_1/src/App.js
@@ -1,24 +1,14 @@
+// root component
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Comp_1 from './Comp_1'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React js</h1>
+      <p>front end development</p>
+      <Comp_1 />
     </div>
   );
 }
