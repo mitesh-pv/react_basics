0959b6cac432227ea36b5e611c569093132ac055
mitesh_pv <mitesh_pv@mitesh-pvs-MacBook-Pro.local>
2019-06-23 01:31:28 +0530

    props: method of passing component from parent to child

--- part_4/app_1/src/App.js
+++ part_4/app_1/src/App.js
@@ -1,6 +1,12 @@
 // root component
+// includes --
+// nested component
+// props:  method of passing one component from parent to child
+
+// use camel case naming convention
+// avoid using underscore in the class names or file names
 import React from 'react';
-import Comp_1 from './Comp_1'
+import Comp1 from './Comp1'
 
 
 function App() {
@@ -8,7 +14,9 @@ function App() {
     <div className="App">
       <h1>Welcome to React js</h1>
       <p>front end development</p>
-      <Comp_1 />
+      <Comp1  name="Steve Rogers" age="30" />
+      <hr/>
+      <Comp1  name="Tony Stark" age="40" />
     </div>
   );
 }
---
+++ part_4/app_1/src/Comp1.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Comp1 extends React.Component{
+
+    render(){
+        console.log(this.props);
+        alert(this.props.name);
+
+        const {name, age}=this.props;
+        return(
+            <div className="comp1">
+                <div>Name: John Wick</div>
+                <div>Age: 30</div>                
+                <br/>
+                <div>New Name: { this.props.name }</div>
+                <div>New Age: { this.props.age }</div>
+                
+                <br/>
+                <div>Const Name: { name }</div>
+                <div>Const Age: { age }</div>
+
+            </div>
+        );
+    }
+}
+
+export default Comp1;
