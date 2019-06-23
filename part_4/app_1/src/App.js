cdca27ff3b22d5287dcbbcb5f4cb8e42ea3b28c5
mitesh_pv <mitesh_pv@mitesh-pvs-MacBook-Pro.local>
2019-06-23 10:53:09 +0530

    passing list properties from parent to child, using map function

--- part_4/app_1/src/App.js
+++ part_4/app_1/src/App.js
@@ -1,24 +1,42 @@
 // root component
 // includes --
-// nested component
-// props:  method of passing one component from parent to child
+// commit 1: nested component
+// commit 2: props:  method of passing one component from parent to child
+// commit 3: passing list of properties from the parent to the child component
+// using the map function to iterate through the list items and print each of them
 
 // use camel case naming convention
 // avoid using underscore in the class names or file names
 import React from 'react';
-import Comp1 from './Comp1'
+import Comp1 from './Comp1';
+import Comp2 from './Comp2';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to React js</h1>
-      <p>front end development</p>
-      <Comp1  name="Steve Rogers" age="30" />
-      <hr/>
-      <Comp1  name="Tony Stark" age="40" />
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    members : [
+      { name: "Steve Rogers", age:"70", id: 1},
+      { name: "Tony Stark", age:"40", id: 2},
+      { name: "Bruce Banner", age:"50", id: 3}
+    ]
+  };
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Welcome to React js</h1>
+        <p>front end development</p>
+        <Comp1  name="Steve Rogers" age="30" />
+        <hr/>
+        <Comp1  name="Tony Stark" age="40" />
+        <hr/>
+        <hr/>
+        <Comp2  members={ this.state.members } />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ part_4/app_1/src/Comp2.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Comp2 extends Component{
+
+    
+    render(){
+    
+        const membersList  = this.props.members.map( eachMember => {
+            return(
+                <div className="membersList" key= { eachMember.id }>
+                    <h1>List items</h1>
+                    <p>Name: { eachMember.name }</p>
+                    <p>Age: { eachMember.age }</p>
+                    <p>ID: { eachMember.id }</p>
+                    
+                </div>
+            );
+        });
+        return(
+            <div className="component-2">
+                { membersList }
+            </div>
+        );
+    }
+}
+
+export default Comp2
