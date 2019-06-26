d41fdcf51ffad69dce430f1202c47445492eb31a
mitesh_pv <mitesh_pv@mitesh-pvs-MacBook-Pro.local>
2019-06-26 19:02:11 +0530

    ToDoList

---
+++ part_5/todolist/src/Comp1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Comp1 extends Component{
+
+    render(){
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        )
+    }
+}
+
+export default Comp1
