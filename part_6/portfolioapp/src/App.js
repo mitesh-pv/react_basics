8543d8bf0229f10aeacd20545d0e852946049c04
mitesh_pv <mitesh_pv@mitesh-pvs-MacBook-Pro.local>
2019-06-27 10:48:12 +0530

    react route dom

--- part_6/portfolioapp/src/App.js
+++ part_6/portfolioapp/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route path="/Home" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+      </div>
+    </BrowserRouter>
   );
 }
 
