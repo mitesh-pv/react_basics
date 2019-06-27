bda51acd3a8c15c082575a0383abdd9e8c79cd8f
mitesh_pv <mitesh_pv@mitesh-pvs-MacBook-Pro.local>
2019-06-27 14:49:14 +0530

    Link and NavLink

--- part_6/portfolioapp/src/components/About.js
+++ part_6/portfolioapp/src/components/About.js
@@ -6,6 +6,7 @@ const About =()=>{
         <div className="container">
             <h4 className="center">About</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo accusamus placeat necessitatibus, nulla consectetur officiis in tenetur assumenda eum consequuntur eveniet voluptate perspiciatis doloribus dolorum sint ullam! Voluptas, temporibus incidunt.</p>
+
         </div>
     )
 }
--- part_6/portfolioapp/src/components/Navbar.js
+++ part_6/portfolioapp/src/components/Navbar.js
@@ -1,20 +1,27 @@
+// using higher order component using react route
+
 import React from 'react'
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
 
-const Navbar =()=>{
+const Navbar =(props)=>{
+    
+    setTimeout(()=>{
+        props.history.push('/About')
+    }, 2000)
     
     return(
         <nav className="nav-wrapper navbar-fixed light-blue accent-3">
             <div className="container">
                 <a  href="./Home" className="brand-logo">JOHN WICK</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/About">About</a></li>
-                    <li><a href="/Contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/About">About</NavLink></li>
+                    <li><NavLink to="/Contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
     )
 }
 
-export default Navbar
+export default withRouter(Navbar); // wrapping the component
